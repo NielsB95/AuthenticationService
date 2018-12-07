5bd1654f09aa4b81ea571e461e945fea48e0c85b
Niels Boerkamp <n.boerkamp@outlook.com>
2018-12-07 21:42:07 +0100

    Added a function which will add all repositories for dependecy injection

--- AuthenticationService.DataLayer/Registration.cs
+++ AuthenticationService.DataLayer/Registration.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AuthenticationService.DataLayer
@@ -7,7 +9,25 @@ namespace AuthenticationService.DataLayer
     {
         public static IServiceCollection AddDataLayer(this IServiceCollection services)
         {
+            services.AddRepositories();
+
             return services;
         }
+
+        private static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            // Get all the repositories
+            var repositories = Assembly
+                        .GetCallingAssembly()
+                        .GetTypes()
+                        .Where(x => x.IsClass)
+                        .Where(x => typeof(IRepository).IsAssignableFrom(x));
+
+            foreach (Type repository in repositories)
+                services.AddSingleton(repository);
+
+            return services;
+        }
+
     }
 }
