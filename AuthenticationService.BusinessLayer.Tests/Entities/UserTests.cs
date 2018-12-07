1a9fc4263031120db47809005c423e08b9d511c5
Niels Boerkamp <n.boerkamp@outlook.com>
2018-12-08 00:04:17 +0100

    Added the EntityValidator and added some unit tests

---
+++ AuthenticationService.BusinessLayer.Tests/Entities/UserTests.cs
@@ -0,0 +1,54 @@
+﻿using AuthenticationService.BusinessLayer.Entities.Users;
+using AuthenticationService.BusinessLayer.Validation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Security;
+
+namespace AuthenticationService.BusinessLayer.Tests.Entities
+{
+	[TestClass]
+	public class UserTests
+	{
+		[TestMethod]
+		public void ValidateInvalidUser()
+		{
+			var user = new User()
+			{
+				Firstname = "John",
+				Lastname = "Doe"
+			};
+
+			var result = EntityValidator.Validate(user);
+
+			// This user object is not valid. Therefore, we expect some errors.
+			Assert.IsTrue(result.Any());
+
+			var messages = result.Select(x => x.ErrorMessage);
+			Assert.IsTrue(messages.Contains("The Username field is required."));
+			Assert.IsTrue(messages.Contains("The Password field is required."));
+		}
+
+		[TestMethod]
+		public void ValidateValidUser()
+		{
+			var password = "secret";
+			var securePassword = new SecureString();
+			foreach (char c in password)
+				securePassword.AppendChar(c);
+
+			var user = new User()
+			{
+				Firstname = "John",
+				Lastname = "Doe",
+				Username = "john",
+				Password = securePassword
+			};
+
+			var result = EntityValidator.Validate(user);
+
+			// This user object is not valid. Therefore, we expect some errors.
+			Assert.IsFalse(result.Any());
+		}
+
+	}
+}
---
+++ AuthenticationService.BusinessLayer/Validation/EntityValidator.cs
@@ -0,0 +1,24 @@
+﻿using AuthenticationService.BusinessLayer.Entities;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationService.BusinessLayer.Validation
+{
+	public class EntityValidator
+	{
+		public static IList<ValidationResult> Validate<T>(T entity) where T : Entity
+		{
+			// Create the context
+			var validationContext = new ValidationContext(entity);
+
+			// Create a list where the results will be sotred in.
+			var validationsResults = new List<ValidationResult>();
+
+			// Validate the entity.
+			bool correct = Validator.TryValidateObject(entity, validationContext, validationsResults, true);
+
+			// Return the list of validation results.
+			return validationsResults;
+		}
+	}
+}
