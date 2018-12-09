18ae19455292e1d5c932f6106facd000ab23fe47
Niels Boerkamp <n.boerkamp@outlook.com>
2018-12-09 14:10:09 +0100

    Added the RoleController

---
+++ AuthenticationService/Controllers/RoleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AuthenticationService.BusinessLayer.Entities.Roles;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationService.Controllers
+{
+    [Route("Roles")]
+    public class RoleController : ControllerBase
+    {
+        private IRoleRepository roleRepository;
+
+        public RoleController(IRoleRepository roleRepository)
+        {
+            this.roleRepository = roleRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IList<Role>>> GetRoles()
+        {
+            return Ok(await roleRepository.GetAll());
+        }
+    }
+}
