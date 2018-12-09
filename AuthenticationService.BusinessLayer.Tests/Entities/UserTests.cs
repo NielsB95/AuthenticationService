f37a0763a45b4a0b028605e664431abdd598a3df
Niels Boerkamp <n.boerkamp@outlook.com>
2018-12-09 12:29:25 +0100

    Fixed unit tests

--- AuthenticationService.BusinessLayer.Tests/Entities/UserTests.cs
+++ AuthenticationService.BusinessLayer.Tests/Entities/UserTests.cs
@@ -6,49 +6,44 @@ using System.Security;
 
 namespace AuthenticationService.BusinessLayer.Tests.Entities
 {
-	[TestClass]
-	public class UserTests
-	{
-		[TestMethod]
-		public void ValidateInvalidUser()
-		{
-			var user = new User()
-			{
-				Firstname = "John",
-				Lastname = "Doe"
-			};
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void ValidateInvalidUser()
+        {
+            var user = new User()
+            {
+                Firstname = "John",
+                Lastname = "Doe"
+            };
 
-			var result = EntityValidator.Validate(user);
+            var result = EntityValidator.Validate(user);
 
-			// This user object is not valid. Therefore, we expect some errors.
-			Assert.IsTrue(result.Any());
+            // This user object is not valid. Therefore, we expect some errors.
+            Assert.IsTrue(result.Any());
 
-			var messages = result.Select(x => x.ErrorMessage);
-			Assert.IsTrue(messages.Contains("The Username field is required."));
-			Assert.IsTrue(messages.Contains("The Password field is required."));
-		}
+            var messages = result.Select(x => x.ErrorMessage);
+            Assert.IsTrue(messages.Contains("The Username field is required."));
+            Assert.IsTrue(messages.Contains("The Password field is required."));
+        }
 
-		[TestMethod]
-		public void ValidateValidUser()
-		{
-			var password = "secret";
-			var securePassword = new SecureString();
-			foreach (char c in password)
-				securePassword.AppendChar(c);
+        [TestMethod]
+        public void ValidateValidUser()
+        {
+            var user = new User()
+            {
+                Firstname = "John",
+                Lastname = "Doe",
+                Username = "john",
+                Password = "secret"
+            };
 
-			var user = new User()
-			{
-				Firstname = "John",
-				Lastname = "Doe",
-				Username = "john",
-				Password = securePassword
-			};
+            var result = EntityValidator.Validate(user);
 
-			var result = EntityValidator.Validate(user);
+            // This user object is valid. Therefore, we expect no errors.
+            Assert.IsFalse(result.Any());
+        }
 
-			// This user object is not valid. Therefore, we expect some errors.
-			Assert.IsFalse(result.Any());
-		}
-
-	}
+    }
 }
