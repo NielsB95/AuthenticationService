10013c0242756e936a2390353c6a43ff4fa7fd10
Niels Boerkamp <n.boerkamp@outlook.com>
2018-12-19 21:20:11 +0100

    Code formatting

--- Backend/AuthenticationService.DataLayer/Repositories/UserRepository.cs
+++ Backend/AuthenticationService.DataLayer/Repositories/UserRepository.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using AuthenticationService.BusinessLayer.Entities.Users;
 using AuthenticationService.DataLayer.Context;
+using System.Linq;
 
 namespace AuthenticationService.DataLayer.Repositories
 {
     public class UserRepository : Repository<User>, IUserRepository
     {
         public UserRepository(AuthenticationServiceContext context) : base(context)
-        {
-        }
+        { }
     }
 }
