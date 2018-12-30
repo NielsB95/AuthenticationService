8dd8e1f3ba5c69ba4dc09b0628cc16d1b93dd212
Niels Boerkamp <n.boerkamp@outlook.com>
2018-12-30 12:27:20 +0100

    Added docker for the front-end

--- Backend/AuthenticationService.DataLayer/Registration.cs
+++ Backend/AuthenticationService.DataLayer/Registration.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using AuthenticationService.BusinessLayer;
 using AuthenticationService.DataLayer.Context;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -92,6 +93,5 @@ namespace AuthenticationService.DataLayer
 
             return services;
         }
-
     }
 }
