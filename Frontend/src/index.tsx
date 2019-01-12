6aa98bc69cf223adb25a58e367978e920b9c2b25
Niels Boerkamp <n.boerkamp@outlook.com>
2019-01-12 11:12:03 +0100

    Enabled the service worker

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -2,5 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Index from './pages/index';
 import './main.css';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Index />, document.querySelector('#root'));
+
+serviceWorker.register();
