f29f09e3d8b134058d29392adadc74198c1ff545
Niels Boerkamp <n.boerkamp@outlook.com>
2019-02-03 14:59:59 +0100

    Code formatting

--- Backend/AuthenticationService.Api/Controllers/DashboardController.cs
+++ Backend/AuthenticationService.Api/Controllers/DashboardController.cs
@@ -5,39 +5,39 @@ using System.Threading.Tasks;
 
 namespace AuthenticationService.Api.Controllers
 {
-    [Route("Dashboard")]
-    [Produces("application/json")]
-    public class DashboardController : ControllerBase
-    {
-        private readonly IDashboardRepository dashboardRepository;
+	[Route("Dashboard")]
+	[Produces("application/json")]
+	public class DashboardController : ControllerBase
+	{
+		private readonly IDashboardRepository dashboardRepository;
 
-        public DashboardController(IDashboardRepository dashboardRepository)
-        {
-            this.dashboardRepository = dashboardRepository;
-        }
+		public DashboardController(IDashboardRepository dashboardRepository)
+		{
+			this.dashboardRepository = dashboardRepository;
+		}
 
-        [HttpGet("UsersFromLastDays")]
-        public async Task<ActionResult<IList<DateValuePair>>> GetUsersFromLastDays()
-        {
-            return Ok(await this.dashboardRepository.GetUsersFromLastDays(14));
-        }
+		[HttpGet("UsersFromLastDays")]
+		public async Task<ActionResult<IList<DateValuePair>>> GetUsersFromLastDays()
+		{
+			return Ok(await this.dashboardRepository.GetUsersFromLastDays(14));
+		}
 
-        [HttpGet("UserActivityFromLastDays")]
-        public async Task<ActionResult<IList<DateValuePair>>> GetActivityFromLastDays()
-        {
-            return Ok(await this.dashboardRepository.GetUserActivityLastDays(14));
-        }
+		[HttpGet("UserActivityFromLastDays")]
+		public async Task<ActionResult<IList<DateValuePair>>> GetActivityFromLastDays()
+		{
+			return Ok(await this.dashboardRepository.GetUserActivityLastDays(14));
+		}
 
-        [HttpGet("FailedAuthenticationActivityLastDays")]
-        public async Task<ActionResult<IList<DateValuePair>>> GetFailedAuthenticationActivityLastDays()
-        {
-            return Ok(await this.dashboardRepository.GetFailedAuthenticationActivityLastDays(14));
-        }
+		[HttpGet("FailedAuthenticationActivityLastDays")]
+		public async Task<ActionResult<IList<DateValuePair>>> GetFailedAuthenticationActivityLastDays()
+		{
+			return Ok(await this.dashboardRepository.GetFailedAuthenticationActivityLastDays(14));
+		}
 
-        [HttpGet("SuccesfulAuthenticationActivityLastDays")]
-        public async Task<ActionResult<IList<DateValuePair>>> GetSuccesfulAuthenticationActivityLastDays()
-        {
-            return Ok(await this.dashboardRepository.GetSuccesfulAuthenticationActivityLastDays(14));
-        }
-    }
+		[HttpGet("SuccesfulAuthenticationActivityLastDays")]
+		public async Task<ActionResult<IList<DateValuePair>>> GetSuccesfulAuthenticationActivityLastDays()
+		{
+			return Ok(await this.dashboardRepository.GetSuccesfulAuthenticationActivityLastDays(14));
+		}
+	}
 }
